09b679b701eced55d338d6ae63c1388eafe7741a
vivekumar58 <vivekiiitalld@gmail.com>
2018-12-19 14:02:57 +0530

    file upload

---
+++ src/main/java/com/demo/rest/FiledownloadApplication.java
@@ -0,0 +1,14 @@
+package com.demo.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiledownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiledownloadApplication.class, args);
+	}
+
+}
+
